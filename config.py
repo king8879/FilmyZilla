22e8eed9e96ad1b48f012c2eb2f94e748aeea38f
king8879 <k4507244@gmail.com>
2025-05-10 16:20:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 
-API_ID = 25617967  # Replace with your actual API ID
-API_HASH = "10555bea1cdfc7d2303fc13b7fd187cc"
+API_ID = 22923037  # Replace with your actual API ID
+API_HASH = "dfb3666878b3851460a58461c5a50f5b"
